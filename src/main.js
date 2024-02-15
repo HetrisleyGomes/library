e3d3db801c6db27e25f1226e48bb4a60d72f5e3c
HetrisleyGomes <hetrisleygomes@gmail.com>
2024-02-15 12:54:31 -0300

    Fix: Erro de segurança
    Acho que corrigi o erro:
    NPM IP package vulnerable to Server-Side Request Forgery (SSRF) attacks

--- src/main.js
+++ src/main.js
@@ -2,4 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+const IP_ADDRESS = ip.address(); 
+function ippublico(){
+    return name === 'public' ? ip.isPrivate(details.address): ip.isPublic(details.address);
+}
 createApp(App).use(router).mount('#app')
